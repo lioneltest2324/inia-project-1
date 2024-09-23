1b989b280b19cf1b8b28b58a131493a213992c7e
lioneltest2324 <120074740+lioneltest2324@users.noreply.github.com>
2024-09-23 22:40:27 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from st_pages import Page, show_pages
+import streamlit as st
+st.set_page_config(layout="wide")
+show_pages(
+    [
+        Page("inia_sep_order_event_track.py", "GA订单用户轨迹")
+
+    ]
+)
