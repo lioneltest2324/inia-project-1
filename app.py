afad7c71482276f8810c937fbb155fde79d9b2ba
lioneltest2324 <120074740+lioneltest2324@users.noreply.github.com>
2024-09-23 23:59:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 show_pages(
     [
-        Page("inia_sep_order_event_track.py", "GA订单用户轨迹")
-
+        Page("inia_sep_order_event_track.py", "GA订单用户轨迹"),
+        Page("google_daily.py", "谷歌日数据")
     ]
 )
