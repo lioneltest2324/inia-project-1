7e92954565e2350b53e7d018563fac7ee0a7cd87
lioneltest2324 <120074740+lioneltest2324@users.noreply.github.com>
2024-09-28 17:39:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(layout="wide")
 show_pages(
     [
-        Page("inia_sep_order_event_track.py", "GA订单用户轨迹"),
+        Page("get_order_by_sku.py", "根据SKU查当日订单"),
+        Page("inia_sep_order_event_track.py", "GA订单用户轨迹(从9月18日开始)"),
         Page("google_daily.py", "谷歌日数据"),
         Page("campaign_custom_compare.py","谷歌系列自定义日期对比数据"),
         Page("google_custom_compare_data.py","谷歌总体自定义日期对比数据"),
