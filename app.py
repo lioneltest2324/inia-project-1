8fb534226bdf4329c2a1255e950aef5c69d9a044
lioneltest2324 <120074740+lioneltest2324@users.noreply.github.com>
2024-10-14 18:48:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ show_pages(
         Page("get_order_by_sku.py", "根据SKU查当日订单"),
         Page("inia_sep_order_event_track.py", "GA订单用户轨迹(从9月18日开始)"),
         Page("google_daily.py", "谷歌日数据"),
+        Page( "google_all_compare_v1.py","谷歌购物广告产品明细"),
         Page("campaign_custom_compare.py","谷歌系列自定义日期对比数据(旧)"),
         Page("google_custom_compare_data.py","谷歌总体自定义日期对比数据(旧)"),
         Page("google-shopping-sku.py","谷歌购物广告产品明细")
